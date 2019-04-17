e7d39bd72c3a19de24ec490030dd609875f68b80
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-04-16 19:51:01 -0500

    Update cp.py
    added argument control

--- tools/python/Linux_Like/cp.py
+++ tools/python/Linux_Like/cp.py
@@ -1,13 +1,15 @@
-from shutil import make_archive, copy2, copytree, copymode, copystat, move, rmtree, copyfile
+from shutil import make_archive, copy2, copytree, move, rmtree, copyfile
 from tempfile import mkdtemp
 from logging import info, basicConfig, INFO
-from os.path import isdir, join
+from os.path import join
 from os import mkdir
+from argparse import ArgumentParser
+
 
 # Check if the wanted archive format can be used also returns it
 def getFormat(path):
     # Reference for any ztar format
-    reference = {'.tar.gz':'gztar', '.tar.bz':'bztar', '.tar.xz':'.xztar'}
+    reference = {'.tar.gz': 'gztar', '.tar.bz': 'bztar', '.tar.xz': '.xztar'}
     if path[-4:] in ('.zip', '.tar'):
         archive_format = path[-3:]
     else:
@@ -18,6 +20,8 @@ def getFormat(path):
     # Debug
     info(f'Using: {archive_format}')
     return archive_format
+
+
 # Handler for the cp command
 class CPHandler:
     def __init__(self):
@@ -31,18 +35,21 @@ class CPHandler:
         self.__preserve = copyfile
         # Copy sources to directory (target)
         self.__targetAsDirectory = None
+
     # Set the destination file
-    def setDest(self, dest):
+    def Dest(self, dest):
         dest = dest.replace('\\', '/')
         if '/' not in dest:
             dest = './' + dest
         self.__dest = dest
+
     # Set the source file
-    def setSource(self, source):
+    def Source(self, source):
         source = source.replace('\\', '/')
         if '/' not in source:
             source = './' + source
         self.__source = source
+
     # Set preserve metadata
     # --preserve[=ATTR_LIST]
     #              preserve the specified attributes (default:
@@ -51,18 +58,21 @@ class CPHandler:
     def setPreserve(self, value):
         if value:
             self.__preserve = copy2
+
     # Set verbose
     # -v, --verbose
     #              explain what is being done
     def setVerbose(self, value):
         if value:
             basicConfig(level=INFO, format='%(message)s')
+
     # Copy all the source files to the folder destination
-    #-t, --target-directory=DIRECTORY
+    # -t, --target-directory=DIRECTORY
     #          copy all SOURCE arguments into DIRECTORY
     def setTargetDirectory(self, value):
         if value:
             self.__targetAsDirectory = value
+
     ## Copy functions
     # Normal copy function for files
     def normal_copy(self):
@@ -103,11 +113,11 @@ class CPHandler:
             # Get the archive format
             archive_format = getFormat(self.__dest)
             # Temporary archive name
-            temporary_name = str(hash(self.__dest)-hash(self.__source))[1:]
+            temporary_name = str(hash(self.__dest) - hash(self.__source))[1:]
             # Temporal archive path
             archive_name = join(temporal_location, temporary_name)
             # Make a archive of the source directory
-            make_archive(archive_name, archive_format, self.__source, self.__source, verbose=self.__verbose,)
+            make_archive(archive_name, archive_format, self.__source, self.__source, verbose=self.__verbose, )
             # When the format wanted is not zip or tar
             if len(archive_format) > 3:
                 archive_format = f'tar.{archive_format[:2]}'
@@ -127,3 +137,46 @@ class CPHandler:
     #              copy directories recursively
     def recursive(self):
         copytree(self.__source, self.__dest, copy_function=self.__preserve)
+
+
+def main(args=None):
+    if not args:
+        parser = ArgumentParser()
+        parser.add_argument('Source', help='Source1;Source2;..SourceN')
+        parser.add_argument('Dest', help='Dest1;Dest2;..DestN')
+        parser.add_argument('-R', '-r', '--recursive', help='Recursive copy of a folder', action='store_const',
+                            const=True, default=False, dest='recursive')
+        parser.add_argument('-a', '--archive',
+                            help='Create archive of a folder in any of this formats (zip,tar,tar.gz,tar.xz,tar.bz)',
+                            action='store_const', const=True, default=False, dest='archive')
+        parser.add_argument('-t', '--target-directory', help='Use the destination as a folder for all the source files', action='store_const',
+                            const=True, default=False, dest='setTargetDirectory')
+        parser.add_argument('-v', '--verbose', dest='setVerbose', help='Set verbose mode', action='store_const',
+                            const=True, default=False)
+        parser.add_argument('--preserve', dest='setPreserve',help='Preserve all metadata of source[s]',action='store_const',
+                            const=True, default=False)
+
+        args = vars(parser.parse_args())
+    # Handler
+    cp_command = CPHandler()
+    # Source[s]
+    source = args['Source']
+    # Dest[s]
+    dest = args['Dest']
+    # Delete non necessary keys
+    del args['Source'], args['Dest']
+    # Set cpy function to use
+    if args['archive']:
+        copy_function = getattr(cp_command, 'archive')
+    elif args['recursive']:
+        copy_function = getattr(cp_command, 'recursove')
+    else:
+        copy_function = getattr(cp_command, 'normal_copy')
+    # Set variables
+    for key in ('setVerbose', 'setPreserve', 'setTargetDirectory'):
+        if args[key]:
+            getattr(cp_command, key)(args[key])
+    # Set all variables
+    copy_function()
+if __name__ == '__main__':
+    main()
