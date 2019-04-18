77068f86bbf829b03851757461b32a3147f9511e
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-04-17 19:06:39 -0500

    Update find.py
    Now find.py the commands that it have are the same of the linux original

--- tools/python/Linux-Like/find.py
+++ tools/python/Linux-Like/find.py
@@ -1,9 +1,7 @@
-from os.path import join, getctime, isdir, getsize
-from os import walk, access, W_OK, R_OK, X_OK, stat, getcwd, listdir
-from sys import platform
-from datetime import datetime
-from threading import Thread
-from argparse import ArgumentParser
+from os.path import join, isdir, getsize
+from os import walk, access, W_OK, R_OK, X_OK, stat, remove
+from shutil import rmtree
+from argparse import ArgumentParser, RawTextHelpFormatter
 from logging import info, INFO, basicConfig
 from re import fullmatch
 from sys import platform
@@ -24,10 +22,12 @@ class Find:
         self.__size = None
         # Type of the wanted file
         self.__type = None
+        # Group of file
+        self.__group = None
         # Filters to apply to the search
         self.__filters = []
-        # Actions to be exectud to results
-        self.__actions = []
+        # Action to be exectud to results
+        self.__action = None
 
     ## Setup
     # Setup the starting point for the search
@@ -91,6 +91,25 @@ class Find:
         if value:
             self.__filters.append(self.__search_writeable)
 
+    # Filter files by it's group
+    def setGroup(self, group):
+        if group:
+            self.__group = group
+            self.__filters.append(self.__search_group)
+
+    ## Set actions values
+    # activate delete action
+    def setDelete(self, value):
+        if value:
+            self.__action = self.__delete
+        else:
+            self.__action = self.__printf
+
+    # Set verbose mode
+    def setVerbose(self, value):
+        if value:
+            basicConfig(level=INFO, format='%(message)s')
+
     ## Filters (conditions)
     # Search by name ignoring lower or upper
     # -name pattern
@@ -136,12 +155,12 @@ class Find:
     # File correspond to a specific group
     # -group gname
     def __search_group(self, path, value):
-        pass
-
-    # Search for permissions if file/folder
-    # -perm mode
-    def __search_perm(self, path, value):
-        pass
+        if platform != 'win32':
+            if getpwuid(stat(join(path, value)).st_gid).pw_name == self.__group:
+                return value
+        else:
+            info('This function is not implemented for windows')
+            return value
 
     # Search if file is readable
     # -readable
@@ -194,6 +213,19 @@ class Find:
             info(str(e))
 
     ## Actions for results
+    # Delete all results
+    def __delete(self, path, value):
+        try:
+            if isdir(join(path, value)):
+                rmtree(join(path, value))
+            else:
+                remove(join(path, value))
+            print(f"Deleted -- {join(path, value)}")
+        except Exception as e:
+            info(str(e))
+
+    def __printf(self, path, value):
+        print(join(path, value))
 
     ## Handlers
     # Handler for filter functions
@@ -210,40 +242,90 @@ class Find:
             buffer.append(group_buffer)
         return buffer
 
+    # Handler for acitons functions
+    def __action_function_handler(self, action_, folder_group):
+        path = folder_group[0]
+        # for group  in ([folders], [files])
+        for group in folder_group[1:]:
+            for value in group:
+                action_(path, value)
+
+    # action handler
+    def __action_handler(self, folder_group):
+        info(f"Action session for {str(folder_group)}")
+        self.__action_function_handler(self.__action, folder_group)
+
     # filter handler
     def __filter_handler(self, folder_group):
         # Pass the folder group for all the filters that were selected
         for filter_ in self.__filters:
+            info(f"Filtering session for {str(folder_group)}")
             folder_group = self.__filter_function_handler(filter_, folder_group)
         return folder_group
 
     # Start find function
     def start(self):
+        info("Find process started")
         results = walk(self.__start_point)
         buffer = []
         # Filtering process
-        print("------Searching with your conditions")
         for folder_group in results:
             buffer.append(self.__filter_handler(folder_group))
-        print("------Printing results")
-        for path, folder_group, file_group in buffer:
-            for folder in folder_group:
-                print(join(path, folder).replace('\\', '/'))
-            for file in file_group:
-                print(join(path, file).replace('\\', '/'))
-        if len(self.__actions) > 0:
-            print("------Doing programmed actions")
+        if self.__action:
+            for folder_group in buffer:
+                self.__action_handler(folder_group)
 
         # Actions to results
         pass
 
 
-basicConfig(level=INFO, format='%(message)s')
-f = Find()
-f.startpoint('./../')
-f.setSize('<1M')
-f.setType('d')
-f.setWrite(1)
-f.setExe(1)
-f.setRead(1)
-f.start()
+def main(args=None):
+    if not args:
+        parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+        parser.add_argument('-name', dest='setName', help='Search for file/folder name ignoring lower and upper',
+                            default=False)
+        parser.add_argument('-iname', dest='setIname',
+                            help='Search for file/folder with lower and upper epecifications', default=False)
+        parser.add_argument('startpoint', help='Folder to start the search')
+        parser.add_argument('-uname', dest='setUser', help='Search by file owner', default=False)
+        parser.add_argument('-empty', dest='setEmpty', help='Search for  empty files and folders (0 bytes size)',
+                            default=False, action='store_const', const=True)
+        parser.add_argument('-size', dest='setSize',
+                            help="Search file by its size using  this sintax \n" \
+                                 "[operator][size][metric] like '<10c'\n" \
+                                 "this will search for files under 10 bytes.\n\n" \
+                                 "Available operators are:\n" \
+                                 "'~' --- files with near size to the specified\n" \
+                                 "'<' --- files with lower size than the spesicied\n" \
+                                 "'>' --- files with higher size than the specified\n" \
+                                 "'=' --- files with the same size as the  specified\n\n" \
+                                 "Available metrics are:\n" \
+                                 "'c' --- size as bytes\n" \
+                                 "'k' --- size as kilobytes\n" \
+                                 "'M' --- size as megabytes\n" \
+                                 "'G' --- size as Gigabytes", default=False)
+        parser.add_argument('-type', dest='setType',
+                            help="Available file types are:\n" \
+                                 "d --- directory\n" \
+                                 "f --- file (only a file with or not with extension)",
+                            default=False)
+        parser.add_argument('-readable', dest='setRead', help='Filter files that are readable by current user',
+                            default=False, action='store_const', const=True)
+        parser.add_argument('-writeable', dest='setWrite', help='Filter files that are writeable by current user',
+                            default=False, action='store_const', const=True)
+        parser.add_argument('-executable', dest='setExe', help='Filter files that are executable by current user',
+                            default=False, action='store_const', const=True)
+        parser.add_argument('-group', dest='setGroup', help='Filter by group', default=False)
+
+        parser.add_argument('-delete', dest='setDelete',
+                            help='Delete all results (use with precaution this will not ask you for every file)',
+                            action='store_const', const=True, default=False)
+        parser.add_argument('-verbose', dest='setVerbose', help='Verbose mode', action='store_const', const=True, default=False)
+        args = vars(parser.parse_args())
+    find = Find()
+    for key in args.keys():
+        getattr(find, key)(args[key])
+    find.start()
+
+if __name__ == '__main__':
+    main()
