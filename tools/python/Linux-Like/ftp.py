59eed32cdbf380b652dc29cb4bc431c921c278a5
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-04-23 20:16:41 -0500

    Update ftp.py
    Now using  ftplib unless the unnecessary low-level complexity of socket module for FTP; now is more readable and raise less amount of exceptions than before

--- tools/python/Linux-Like/ftp.py
+++ tools/python/Linux-Like/ftp.py
@@ -1,220 +1,193 @@
-from socket import socket, AF_INET, AF_INET6, SOCK_STREAM
-from re import match
+import ftplib
 from argparse import ArgumentParser
-
-
-# Printer for responses
-def print_response(response):
-    if response:
-        print(response.decode())
-
-
-# Find every file in a list like the one of "ls" command
-def find_posible_file(pattern, ls_list):  # NOT IMPLEMENTED
-    m = match(pattern, ls_list)
-
-
-# Get command/code and args
-def getCodeOrCommand(cmd):
-    return cmd.split(' ')
-
-
-# Handler for the FTP session
-class FTPHandler:
-    def __init__(self, address, client):
-        self.__host, self.__port = address
-
-        self.__client = client  # Socket for command communication
-        self.__logged_in = False  # Useful handler to limit the client and prevent crash by not executing certain cmds before we are logged in
-        self.__timeout = 1  # The timeout for the recv
-        self.__is_active = False
-
-    # Passive eFTP connection
-    def __pasv(self):
-        self.__client.send(b'PASV\r\n')
-        # Receive port to connect
-        cmd = self.__recv(self.__client).decode()
-        # Separate the  args from the PORT command
-        code, *args = getCodeOrCommand(cmd)
-        # When the server have implemented it
-        pre_port = args[-1].replace('(', '').replace(')', '').replace('.', '').strip().split(',')
-        port = int(pre_port[4]) * 256 + int(pre_port[5])
-        # Connect with the received port
-        data_client = socket(AF_INET, SOCK_STREAM)
-        address = ('.'.join(pre_port[:4]), port)
-        data_client.connect(address)
-        return data_client
-
-    # get command from string
-    def __getCommandString(self, cmd):
-        try:
-            return getattr(self, cmd)
-        except:
-            return None
-
-    # Recieve data of any size
-    def __recv(self, client):
-        if client is not None:
-            client.settimeout(self.__timeout)
-            try:
-                buffer = b''
-                while True:
-                    data = client.recv(1024)
-                    if len(data) < 1024:
-                        # data send loop completed
-                        client.settimeout(3600)
-                        return buffer + data
-                    buffer += data
-            except Exception as e:
-                client.settimeout(3600)
-                return None
-
-    # Check the code of the response
-    def __check_code(self, msg):
-        codes = {'231': "QUIT", '221': "QUIT", '214': 'SUCCESSFUL', '230': 'LOGGED', '500': 'ERROR',
-                 '200': 'SUCCESSFUL'}
-        code = msg[:3]
-        if code in ['221', '231']:
-            exit(-1)
-        elif code == '230':
-            self.__logged_in = True
-        elif code[0] == '5':
-            return False
-        else:
-            return True
-
-    ## Add more function bellow here
-    # Best way to use LIST command
-    def ls(self, args, return_list=False):
-        data_client = self.__pasv()
-        msg = (f"LIST " + " ".join(args) + "\r\n").encode()
-        self.__client.send(msg)
-        ls_list = self.__recv(data_client)
-        data_client.close()
-        if return_list:
-            return ls_list
-        else:
-            print_response(self.__recv(self.__client))
-            print_response(ls_list)
-            print_response(self.__recv(self.__client))
-
-    # Download a file using RETR
-    ## Implement the retr and wait for data in the data_client port
-    def retr(self, args):
-        data_client = self.__pasv()
-        msg = f"RETR {args[0]}\r\n".encode()
-        filename = args[0].replace('\\', '/').split('/')[-1]
-        self.__client.send(msg)
-        response1 = self.__recv(self.__client)
-        # Start recieving
-        if response1:
-            # If the code is an error
-            if self.__check_code(response1):
-                print(response1.decode().strip())
-                # File content
-                file_content = self.__recv(data_client)
-
-                print_response(self.__recv(self.__client))
-                if file_content:
-                    with open(filename, 'wb') as file:
-                        file.write(file_content)
-                        file.close()
-        data_client.close()
-
-    # Equivalent to get from FTP client in linux
-    def get(self, args):
-        self.retr(args)
-
-    def mget(self, args):
-        print("NOT IMPLEMENTED YET")
-
-    # Start the connection
-    def start_connection(self):
-        welcome = self.__recv(self.__client).decode()
-        print(welcome.strip())
-        while True:
-            msg = input('ftp>')
-            # If not data will be send, send NOOP command (no operation)
-            if not len(msg):
-                msg = 'NOOP'
-            # Separate authenticated session of not ayth session
-            ## this let us to except an y socket block during the session
-            if self.__logged_in:
-                cmd, *args = getCodeOrCommand(msg)
-                commandFunction = self.__getCommandString(cmd.lower())  # Method/Function of the string
-                # Only execute command if is one of the allowed
-                if commandFunction and cmd != 'start_connection':
-                    commandFunction(args)  # Execute one implemented command
-                # if no command have been found only send it raw
-                else:
-                    # Client for sending all the data (data port)
-                    data_client = self.__pasv()
-                    msg = (msg + '\r\n').encode()
-                    self.__client.send(msg)
-                    response1 = self.__recv(self.__client)
-                    if response1:
-                        print(response1.decode().strip())
-                        # Only try to print the other responses if we got a successful code as response
-                        if self.__check_code(response1.decode()):
-                            print_response(self.__recv(data_client))
-                            print_response(self.__recv(self.__client))
-                    data_client.close()
-
-            else:
-                # Simple socket communication for not authenticated session
-                msg = (msg + '\r\n').encode()
-                self.__client.send(msg)
-                response = self.__recv(self.__client).decode()
-                self.__check_code(response)
-                print(response.strip())
-
-
-# FTP Client
+# Function to download files
+def download(filename):
+    # This works for retrbinary()
+    def write_bytes(btes):
+        with open(filename, 'wb') as file:
+            file.write(btes)
+            file.close()
+    return write_bytes
+# FTP session handler
 class FTP:
     def __init__(self):
-        self.__host = None
-        self.__port = None
-        self.__family = None
-        self.__client = None
+        self.__session = ftplib.FTP()
+        self.__address = [None, None]
+        self.__mode = True
+    ## Setup options
+    #  Interactively change mode
+    def changemode(self, args):
+        if not args:
+            mode = input('Do you want a pasv connection Y/n').strip()
+            if not len(mode) or 'y' in mode.lower():
+                # PASV mode
+                mode = True
+            else:
+                # ACTV mode
+                mode = False
+        else:
+            mode = {'PASV':True, 'ACTV':False}[args[0]]
+        # Stablish the mode
+        self.__session.set_pasv(mode)
+    def HOST(self, value):
+        self.__address[0] = value
+    def PORT(self, value):
+        self.__address[1] = value
+    ## Commands implemented
+    # Login into the server interactively
+    def login(self, args):
+        # Login as anonymous
+        if '-a' in args:
+            self.__session.login()
+        # When we login as a specific user
+        elif len(args) == 3:
+            self.__session.login(args[1], args[2])
+        # Interactive mode
+        else:
+            while True:
+                print('Interactive login')
+                # Try to log in until we got it
+                try:
+                    # When we only provide a username
+                    if len(args) < 2:
+                        username = input('Username: ').strip()
+                    else:
+                        username = args[1]
+                    password = input('Password: ').strip()
+                    self.__session.login(username, password)
+                    print('Logged in')
+                    break
+                except:
+                    print('Login failed')
+    # ls command ftp implementation
+    def ls(self, args):
+        args[0] = 'LIST'
+        self.other(args)
+    # cd command
+    def cd(self, args):
+        args[0] = 'CWD'
+        self.other(args)
+    # Connect to target if no one was specified before
+    def open(self, args):
+        # Interactive
+        if not args or len(args) <= 1:
+            args = ['open'] + input('to:').strip().split(' ')
+            # When no port is provided we use 21 as default
+        if len(args) <= 2:
+            args.append(21)
+        host, port = args[1], int(args[2])
+        self.__session.connect(host, port)
+        print(self.__session.welcome)
 
-    # SEt the familyof the command socket
-    def setfamily(self, family):
-        self.__family = family
+    def rename(self, args):
+        # argv mode
+        if len(args) == 3:
+            old_name = args[1]
+            new_name = args[2]
+        else:
+            # Interactive mode
+            if len(args) == 1:
+                old_name = input('Old filename:').strip()
+            if len(args) <= 2:
+                new_name = input('New filename: ').strip()
+        # Send request for change filename
+        self.__session.sendcmd(f'RNFR {old_name}')
+        # Send new filename
+        print(self.__session.sendcmd(f'RNTO {new_name}'))
 
-    # Set host
-    def host(self, host):
-        self.__host = host
-
-    # Set port
-    def port(self, port):
-        self.__port = port
-
-    # Create connection
-    def __create_client(self):
-        self.__client = socket(self.__family, SOCK_STREAM)
-        self.__client.connect((self.__host, self.__port))
-
-    # Start the client after setting up everything
+    # multiple puts
+    def mput(self, args):
+        for arg in args[1:]:
+            # this is only a loop operation of put
+            try:
+                self.put([None, arg]) # by default put only accept iterables
+            except Exception as e:
+                print(e)
+    # quit; exit from the program when quit is called
+    def quit(self, args):
+        try:
+            self.other(args)
+        except Exception as e:
+            print(e)
+        exit(-1)
+    # Classic upload command
+    def put(self, args):
+        # File object to get bytes
+        file = open(args[1], 'rb')
+        # Send the bytes of the file object
+        response = self.__session.storbinary(f'STOR {args[1]}', file)
+        file.close()
+        print(response)
+    # Classic download command in ftp
+    def get(self, args):
+        # In this case args is filename
+        filename = args[1].replace('\\', '/').split('/')[-1]
+        self.__session.retrbinary(f'RETR {args[1]}', download(filename))
+    # execute any other command
+    def other(self, args):
+        # Except not implemented cmds
+        try:
+            cmd = ' '.join(args)
+            if 'ls' in args[0][:3].lower() or 'list' in args[0].lower():
+                print(self.__session.retrlines(cmd))
+            else:
+                print(self.__session.sendcmd(cmd))
+        except Exception as e:
+            print(e)
+    # Remove file or directory (directory recursively)
+    def rm(self, args):
+        # Remove Tree
+        if '-r' in args:
+            # Do not send this argument
+            args.remove('-r')
+            args[0] = 'RMDA'
+        # Remove file
+        else:
+            args[0] = 'DELE'
+        self.other(args[:2])
+    # Remove empty directory
+    def rmdir(self, args):
+        args[0] = 'RMD'
+        self.other(args)
+    # Make directory command
+    def mkdir(self, args):
+        args[0] = 'MKD'
+        self.other(args)
     def start(self):
-        self.__create_client()
-        # Create the handlerr for the session
-        handler = FTPHandler((self.__host, self.__port), self.__client)
-        # Start the handler
-        handler.start_connection()
-
-
+        # Connect to server if arguments was provided in argv
+        if all(self.__address):
+            self.open(['open',*self.__address])
+        # Start interactive session
+        while True:
+            cmd = input('ftp>').strip()
+            # Never send empty data
+            if not len(cmd):
+                cmd = 'NOOP'
+            # Always store the cmd splited
+            cmd = cmd.split(' ')
+            # Depureate spaces
+            cmd = list(filter(len, cmd))
+            # if the command was implemented
+            try:
+                getattr(self, cmd[0])(cmd)
+            except Exception as e:
+                print(e)
+                # Try to send it as a normal cmd
+                self.other(cmd)
+# main function to execute the script
 def main(args=None):
     if not args:
         parser = ArgumentParser()
-        parser.add_argument('HOST', help='HOST IP')
-        parser.add_argument('PORT', help='PORT of the FTP server', default=21, type=int)
-        parser.add_argument('-6', dest='setfamily', help='User IPV6', action='store_const', const=AF_INET6, default=AF_INET)
-        args = vars(parser.parse_args())
+        parser.add_argument('HOST', help='Host ip')
+        parser.add_argument('PORT', help='HOST port', default=21, type=int)
+        parser.add_argument('-A', help='Use active mode', default=False, action='store_const', const=['ACTV'], dest='changemode')
+        parser.add_argument('-p', help='Use pasv mode', default=False, action='store_const', const=['PASV'], dest='changemode')
+        args =  vars(parser.parse_args())
+    # FTP session handler
     ftp = FTP()
     for key in args.keys():
-        getattr(ftp, key.lower())(args[key])
+        if args[key]:
+            getattr(ftp, key)(args[key])
     ftp.start()
-
-
 if __name__ == '__main__':
     main()
