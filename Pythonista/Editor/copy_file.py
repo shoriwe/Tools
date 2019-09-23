4fbb09e8760d094666569e4c79c1cd29bfd48314
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-09-22 23:17:47 -0500

    Update copy_file.py
    Reformated the file

--- Pythonista/Editor/copy_file.py
+++ Pythonista/Editor/copy_file.py
@@ -5,9 +5,10 @@ from shutil import copy2, copytree
 from os import listdir, getcwd
 from appex import get_file_paths
 
+
 # This function is better that the original from shutil
 # Thanks to @atzz from StackOverflow
-def betterCopyTree(src, dst, symlinks=False, ignore=None):
+def better_copytree(src, dst, symlinks=False, ignore=None):
     for item in listdir(src):
         s = join(src, item)
         d = join(dst, item)
@@ -15,8 +16,10 @@ def betterCopyTree(src, dst, symlinks=False, ignore=None):
             copytree(s, d, symlinks, ignore)
         else:
             copy2(s, d)
+
+
 # Faster join and prevent join "step back directories" and "current working directories"
-def betterJoin(base, target):
+def better_join(base, target):
     if target == '..':
         path = '/'.join(base.split('/')[:-1])
     elif target == '.':
@@ -27,13 +30,14 @@ def betterJoin(base, target):
         path = path[:-1]
     return path
 
+
 # Function that is called to change the current directory
 # it is callled by "Back" button and when the user selects a row of the tableview
 def cd(sender):
     if sender.title == 'Back':
         # this ide is called when user press "Back" button
         current_dir = view.data_source.current_dir
-        view.data_source.update_items(betterJoin(current_dir, '..'))
+        view.data_source.update_items(better_join(current_dir, '..'))
     else:
         # This side is called when the user selects a row
         row = view.selected_row[1]
@@ -41,12 +45,13 @@ def cd(sender):
         item = view.data_source.items[row]
         # Use the current dir to create a new complete path to the selected directory
         current_dir = view.data_source.current_dir
-        view.data_source.update_items(betterJoin(current_dir, item))
+        view.data_source.update_items(better_join(current_dir, item))
     # Always reload the view (this updates the tableview values)
     view.reload()
 
+
 # Quicker and cleanest way to create a button
-def buttonCreator(title, action, frame, color):
+def button_creator(title, action, frame, color):
     button = ui.Button()
     button.title = title
     button.action = action
@@ -58,18 +63,22 @@ def buttonCreator(title, action, frame, color):
     button.tint_color = '#000000'
     return button
 
+
 # This delegate help the table view with it's  selected rows
 class TableViewDelegate(object):
     # Function that is executed every time a row is selected
     def tableview_did_select(self, tableview, section, row):
         cd(tableview)
 
+
 # Get the home directory
 def get_home():
     pwd = getcwd()
     while len(pwd.split("/")) > 8:
-        pwd = betterJoin(pwd, "..")
+        pwd = better_join(pwd, "..")
     return pwd
+
+
 # Data source
 # For some reason tableview requires a object to manage it's values.
 # This only grabs values and make it acceible for the tableview
@@ -87,7 +96,7 @@ class CustomDataSource(object):
         self.update_items(pwd)
         # This three values are used for the script frames
         screen_size = ui.get_screen_size()
-        cell = ui.TableViewCell() # this really is only necessary for the calculation of the copy button frame but is
+        cell = ui.TableViewCell()  # this really is only necessary for the calculation of the copy button frame but is
         # never used
         # This is used for the calculation of the cellframe
         cell_frame = cell.frame
@@ -95,16 +104,20 @@ class CustomDataSource(object):
         self.disabled_buttons = []
         # Reference frame for the cell
         self.button_cell_frame_ref = (
-        (screen_size[0] * 320) / 414, (cell_frame[3] * 15) / 44, (cell_frame[2] * 100) / 414, (cell_frame[3] * 30) / 44)
+            (screen_size[0] * 320) / 414, (cell_frame[3] * 15) / 44, (cell_frame[2] * 100) / 414,
+            (cell_frame[3] * 30) / 44)
+
     # Update the values of the items with the content of the current directory
     def update_items(self, pwd):
-        self.items = list(filter(lambda file: isdir(betterJoin(pwd, file)), listdir(pwd)))
+        self.items = list(filter(lambda file: isdir(better_join(pwd, file)), listdir(pwd)))
         # update  the new current directory
         self.current_dir = pwd
+
     #  Creation of the button for copy files
-    def copyhereButton(self):
-        button = buttonCreator('Copy here', self.copy, self.button_cell_frame_ref, '#80f228')
+    def copyhere_button(self):
+        button = button_creator('Copy here', self.copy, self.button_cell_frame_ref, '#80f228')
         return button
+
     # Copy function called by "Copy here" button
     def copy(self, sender):
         item = sender.superview.text_label.text
@@ -118,12 +131,12 @@ class CustomDataSource(object):
         #  for each file the user want to copy
         for file in args:
             # Complete path to target folder
-            path = betterJoin(self.current_dir, item)
+            path = better_join(self.current_dir, item)
             # When the source file is a folder
             if isdir(file):
-                target_folder = file.split('/')[-1] # Copytree requires a not existing folder as target
+                target_folder = file.split('/')[-1]  # Copytree requires a not existing folder as target
                 # Copy the fie
-                betterCopyTree(file, betterJoin(path, target_folder))
+                better_copytree(file, better_join(path, target_folder))
             else:
                 # When source is a file
                 copy2(file, path)
@@ -131,6 +144,7 @@ class CustomDataSource(object):
         sender.enabled = False
         # add the button
         self.disabled_buttons.append(path)
+
     # Function that creates eaach cell for the tableview
     def tableview_cell_for_row(self, tableview, section, row):
         # Cell object that is added as subview to the tableview
@@ -138,16 +152,18 @@ class CustomDataSource(object):
         # Add the cell the element corresponding with the row gived
         cell.text_label.text = self.items[row]
         # Button that is added to the cell (this is the "Copy here" button)
-        button = self.copyhereButton()
+        button = self.copyhere_button()
         # Check if the button was disabled before
-        if betterJoin(self.current_dir, self.items[row]) in self.disabled_buttons:
+        if better_join(self.current_dir, self.items[row]) in self.disabled_buttons:
             button.enabled = False
         # Finally add the button as a subview of the cell and return the cell
         cell.add_subview(button)
         return cell
+
     # Function necessary for the construction of the data source it returns the len of the items list
     def tableview_number_of_rows(self, tableview, section):
         return len(self.items)
+
 
 # This is the subview
 view = ui.TableView()
